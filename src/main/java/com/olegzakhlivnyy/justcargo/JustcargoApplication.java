1fd29787565807b639be6a0ce6b8040d7f14c5ef
OZakhlivny <oleg.zakhlivny@gmail.com>
2021-11-24 12:37:15 +0100

    Initial commit

---
+++ src/main/java/com/olegzakhlivnyy/justcargo/JustcargoApplication.java
@@ -0,0 +1,13 @@
+package com.olegzakhlivnyy.justcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustcargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustcargoApplication.class, args);
+	}
+
+}
